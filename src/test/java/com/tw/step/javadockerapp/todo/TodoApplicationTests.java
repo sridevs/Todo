4ed3bef72359645774af32b7cf05a20fcf22ed64
sridevs <sridevs@thoughtworks.com>
2019-02-25 09:30:42 +0530

    Added postgres-embedded for db connection in tests

--- src/test/java/com/tw/step/javadockerapp/todo/TodoApplicationTests.java
+++ src/test/java/com/tw/step/javadockerapp/todo/TodoApplicationTests.java
@@ -3,14 +3,17 @@ package com.tw.step.javadockerapp.todo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = {DbConfig.class})
 public class TodoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
